af42e2ed9eddf4be989df75381af721b2750e832
bbviana <bbviana@touchhealth.com.br>
2013-09-09 20:08:34 -0300

    admin
    Change-Id: I0598321ca159c624954e40ceb167209994bd361c

---
+++ touch-games-web/src/main/java/br/com/touchtec/games/web/controller/AdminAction.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 1999-2009 Touch Tecnologia e Informatica Ltda.
+ * Gomes de Carvalho, 1666, 3o. Andar, Vila Olimpia, Sao Paulo, SP, Brasil.
+ * Todos os direitos reservados.
+ * 
+ * Este software e confidencial e de propriedade da Touch Tecnologia e
+ * Informatica Ltda. (Informacao Confidencial). As informacoes contidas neste
+ * arquivo nao podem ser publicadas, e seu uso esta limitado de acordo com os
+ * termos do contrato de licenca.
+ */
+
+package br.com.touchtec.games.web.controller;
+
+
+import br.com.touchtec.twf.core.TWFActionSupport;
+
+
+/**
+ * @author bbviana
+ */
+public class AdminAction extends TWFActionSupport {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public String execute() throws Exception {
+        return "jsp/admin/admin";
+    }
+
+}
