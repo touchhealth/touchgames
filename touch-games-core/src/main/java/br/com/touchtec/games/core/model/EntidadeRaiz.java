d7c10f90f77091b84973a00e935987d6342ec82d
bbviana <bbviana@touchhealth.com.br>
2013-09-11 18:13:46 -0300

    formatação
    Change-Id: I977db66bef71425f1a083e19d685887681fb2a0f

--- touch-games-core/src/main/java/br/com/touchtec/games/core/model/EntidadeRaiz.java
+++ touch-games-core/src/main/java/br/com/touchtec/games/core/model/EntidadeRaiz.java
@@ -66,8 +66,7 @@ public abstract class EntidadeRaiz implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = (prime * result)
-                + ((this.id == null) ? 0 : this.id.hashCode());
+        result = (prime * result) + ((this.id == null) ? 0 : this.id.hashCode());
         return result;
     }
 
